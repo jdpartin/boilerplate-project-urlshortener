5e3682c6f23bd76dd7041710b27e0e75e9126a13
Jacob Partin <jdpartin@icloud.com>
2024-10-28 00:49:50 +0000

    Initial commit

--- index.js
+++ index.js
@@ -19,6 +19,61 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// URL Shortener
+const shortUrls = [];
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const dns = require('dns');
+const urlParser = require('url');
+
+app.post('/api/shorturl/', (req, res) => {
+  const originalUrl = req.body.url;
+  let parsedUrl;
+
+  try
+  {
+    parsedUrl = new URL(originalUrl);
+  } 
+  catch (err)
+  {
+    return res.json({ error: 'invalid url' });
+  }
+
+  dns.lookup(parsedUrl.hostname, (err) => {
+    if (err)
+    {
+      return res.json({ error: 'invalid url' });
+    }
+
+    const shortUrlId = getShortUrl(parsedUrl);
+
+    res.json({ original_url: originalUrl, short_url: shortUrlId });
+  });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrlId = req.params.short_url;
+  
+  const originalUrl = shortUrls[parseInt(shortUrlId)];
+  
+  if (originalUrl)
+  {
+    res.redirect(originalUrl);
+  }
+  else
+  {
+    res.json({ error: 'No short URL found' });
+  }
+});
+
+function getShortUrl(url)
+{
+  shortUrls.push(url);
+  return shortUrls.length - 1;// The index is the id
+}
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
